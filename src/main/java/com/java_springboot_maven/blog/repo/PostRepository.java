8239a7a1a5d59ec7aedb61ed16d3ab272a864268
varanitsa <varanitsa.es@gmail.com>
2022-06-09 15:28:25 +0200

    blog-details html  added ,@GeMapping for blog by ID

--- src/main/java/com/java_springboot_maven/blog/repo/PostRepository.java
+++ src/main/java/com/java_springboot_maven/blog/repo/PostRepository.java
@@ -3,5 +3,8 @@ package com.java_springboot_maven.blog.repo;
 import com.java_springboot_maven.blog.models.Post;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
+    Optional<Post> findAllById(long id);
 }
